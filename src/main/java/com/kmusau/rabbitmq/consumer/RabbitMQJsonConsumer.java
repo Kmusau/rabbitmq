69fd491365a88bf78cc7edbec361950d52158d9e
Kmusau <kevomusa6565@gmail.com>
2023-06-13 16:00:22 +0300

    Added jsom message Consumer :rocket:

--- src/main/java/com/kmusau/rabbitmq/consumer/RabbitMQJsonConsumer.java
+++ src/main/java/com/kmusau/rabbitmq/consumer/RabbitMQJsonConsumer.java
@@ -1,4 +1,16 @@
 package com.kmusau.rabbitmq.consumer;
 
+import com.kmusau.rabbitmq.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class RabbitMQJsonConsumer {
+
+    @RabbitListener(queues = {"${rabbitmq.json.queue.name}"})
+    public void consumeJsonMessage(UserDto userDto) {
+        log.info("Received user object :: {}", userDto);
+    }
 }
